f4943d008fb64ef6cf4d2df0edcff8b12ae495e8
AaronsLifeGame <alpoole05@gmail.com>
2021-09-18 08:39:51 +1000

    | Edit
    Trying Again `rainbow.js`

--- src/Commands/rainbow.js
+++ src/Commands/rainbow.js
@@ -8,52 +8,21 @@ module.exports = new Command({
 
     async run(message, args, client) {
         const embed = new Discord.MessageEmbed();
-            embed
-            .setColor("#000000")
-            .setDescription("RAINBOW!")
+        
+        embed
+        .setColor("#000000")
+        .setDescription("RAINBOW!")
 
-            message.send({embeds: [embed]});
+        message.channel.send({ embeds: [embed] })
 
-        const sentEmbed = await message.channel.send({ embeds: [embed] })
-            embed
-            .setColor("#ED4245")
-            .setDescription("RAINBOW!")
+        setTimeout(() => message.delete(), 1000);
+        
+        embed
+        .setColor("#FFFFFF")
+        .setDescription("RAINBOW!")
 
-            sentEmbed.edit({ embeds: [embed] })
+        message.channel.send({ embeds: [embed] });
 
-        const sentEmbed = await message.channel.send({ embeds: [embed] })
-            embed
-            .setColor("#FEE75C")
-            .setDescription("RAINBOW!")
-
-        sentEmbed.edit({ embeds: [embed] })
-
-        const sentEmbed = await message.channel.send({ embeds: [embed] })
-            embed
-            .setColor("#57F287")
-            .setDescription("RAINBOW!")
-
-        sentEmbed.edit({ embeds: [embed] })
-
-        const sentEmbed = await message.channel.send({ embeds: [embed] })
-            embed
-            .setColor("#5865F2")
-            .setDescription("RAINBOW!")
-
-        sentEmbed.edit({ embeds: [embed] })
-
-        const sentEmbed = await message.channel.send({ embeds: [embed] })
-            embed
-            .setColor("#EB459E")
-            .setDescription("RAINBOW!")
-
-        sentEmbed.edit({ embeds: [embed] })
-
-        const sentEmbed = await message.channel.send({ embeds: [embed] })
-            embed
-            .setColor("#FFFFFF")
-            .setDescription("RAINBOW!")
-
-        sentEmbed.edit({ embeds: [embed] })
+        setTimeout(() => message.delete(), 1000);
     }
 })
