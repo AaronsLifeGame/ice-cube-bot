f5c0f5915cbd51facbb421c4e173cdf534098d60
AaronsLifeGame <alpoole05@gmail.com>
2021-09-18 17:18:32 +1000

    | Edit

--- src/Commands/embed.js
+++ src/Commands/embed.js
@@ -12,7 +12,7 @@ module.exports = new Command({
 		embed
 		.setColor("#5865F2")
 		.setTitle("Some title")
-		.setAuthor(message.author.username, message.author.avatarURL({dynamic: true}), 
+		.setAuthor(message.author.username, message.author.avatarURL({dynamic: true}) 
 			"https://dsc.gg/chillzone2.0/")
 		.setDescription("Some description here, \n[Invite Link](https://dsc.gg/chillzone2.0/)")
 		.setThumbnail("https://i.imgur.com/AfFp7pu.png")
