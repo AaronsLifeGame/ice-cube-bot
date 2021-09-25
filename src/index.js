001bf903895d13c5d496635f2b3568be00222c95
AaronsLifeGame <alpoole05@gmail.com>
2021-09-25 18:18:12 +1000

    | Edit

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ const Client = require("./Structures/Client.js");
 const config = require("./Data/config.json");
 const client = new Client();
 
+const distube = require("distube")
+client.distube = new Distube(client, { searchSongs: false, emitNewSongOnly: true});
+client.distube
+	.on("playSong", (message, queue, song) => message.channel.send(
+		`Playing **${song.name}** - \`${song.formattedDuration}\`\nRequested by: ${song.user}`
+	))
+
 client.start(process.env.TOKEN);
