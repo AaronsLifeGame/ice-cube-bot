63d347a5ae400800bfc54f8f385d1954f7b1dc02
AaronsLifeGame <alpoole05@gmail.com>
2021-09-26 10:41:47 +1000

    | Edit

--- src/Commands/hello.js
+++ src/Commands/hello.js
@@ -3,8 +3,6 @@ const Command = require("../Structures/Command.js");
 module.exports = new Command({
 	name: "hello",
 	description: "Hello!",
-	type: "BOTH",
-	slashCommandOptions: [],
 	permission: "SEND_MESSAGES",
 	
 	async run(message, args, client) {
--- src/Commands/kick.js
+++ src/Commands/kick.js
@@ -6,35 +6,10 @@ module.exports = new Command({
 	description: "Kicks the Mentioned User!",
 	type: "SLASH",
 	slashCommandOptions: [{
-		name: "target",
-		description: "The Target you want to Kick!",
+		name: "user",
+		description: "The User you want to Kick!",
 		type: "USER",
 		required: true
 	}],
 	permission: "MANAGE_MESSAGES",
-	async run(message, args, client) {
-		const amount = args[1];
-
-		if (!amount || isNaN(amount))
-			return message.reply(
-				`${
-					amount == undefined ? "Nothing" : amount
-				} is not a valid number!`
-			);
-
-		const amountParsed = parseInt(amount);
-
-		if (amountParsed > 100)
-			return message.reply("You cannot clear more than 100 messages!");
-
-		message.channel.bulkDelete(amountParsed);
-
-		const msg = await message.reply(
-			`> Cleared ${amountParsed} messages!`
-		);
-
-		
-
-		if (msg) setTimeout(() => msg.delete(), 5000);
-	}
-});
+	
