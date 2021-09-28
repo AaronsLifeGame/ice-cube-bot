27e6b9d067485334e74b4a745f10ab9aaf6d4c3d
AaronsLifeGame <alpoole05@gmail.com>
2021-09-28 17:37:14 +1000

    | Fix

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ require('dotenv').config();
 
 const Client = require("./Structures/Client.js");
 const config = require("./Data/config.json");
-const client = new Client();
 
 // Discord Player..
 	const { Player } = require('discord-player');
-	const { Client, Intents } = require('discord.js');
-
+	
 	global.client = new Client({
 	    intents: [
 	        Intents.FLAGS.GUILDS,
