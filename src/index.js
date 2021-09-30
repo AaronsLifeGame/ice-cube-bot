ae7a79091e5731879319de78b04a2750857d5673
AaronsLifeGame <alpoole05@gmail.com>
2021-09-30 11:36:13 +1000

    | edit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ require('dotenv').config();
 
 const { Client, Intents } = require("./Structures/Client.js");
 const config = require("./Data/config.json");
-const client = new Client();
 
 const { Player } = require('discord-player');
+
+global.client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ],
+    disableMentions: 'everyone',
+});
 
 client.config = require('./config');
 
 global.player = new Player(client, client.config.opt.discordPlayer);
 
-require('./loader');
-require('./events');
+require('./src/loader');
+require('./src/events');
 
 client.start(process.env.TOKEN);
