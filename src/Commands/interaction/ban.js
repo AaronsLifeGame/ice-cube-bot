022c0b20279eae35040893cb9d7985332e0fa5b4
AaronsLifeGame <alpoole05@gmail.com>
2021-10-01 11:40:47 +1000

    | Test

--- src/Commands/interaction/ban.js
+++ src/Commands/interaction/ban.js
@@ -21,8 +21,8 @@ module.exports = new Command({
 	permission: "MANAGE_MEMBERS",
 
 	async run(message, args, client) {
-		const user = interaction.slashCommandOptions.getUser("user")
-		let Reason = interaction.slashCommandOptions.getString("reason")
+		const user = interaction.options.getTarget("user")
+		let Reason = interaction.options.getString("reason")
 		const member = interaction.guild.members.cacheget(user.id)
 		
 		if(!member) return interaction.reply("> The User doesn't exist in this Server!")
