2b22431cf135d3f09dbc1941273439f21a84a548
AaronsLifeGame <alpoole05@gmail.com>
2021-10-05 10:27:59 +1100

    | Edit

---
+++ src/Commands/command/dBump.js
@@ -0,0 +1,7 @@
+const Command = require("../../Structures/Command.js");
+const Discord = require("discord.js");
+
+module.exports = new Command({
+    name: "d bump",
+    permission: "SEND_MESSAGES",
+})
