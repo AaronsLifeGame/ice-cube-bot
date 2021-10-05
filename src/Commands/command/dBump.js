583b8a82adcd153bcfd401b45781d35742888f45
AaronsLifeGame <alpoole05@gmail.com>
2021-10-05 11:12:06 +1100

    | Fix

--- src/Commands/command/dBump.js
+++ src/Commands/command/dBump.js
@@ -4,4 +4,15 @@ const Discord = require("discord.js");
 module.exports = new Command({
     name: "d",
     permission: "SEND_MESSAGES",
+
+    async run(message, args, client) {
+        const embed = new Discord.MessageEmbed();
+
+            embed
+            .setColor("#000000")
+            .setTitle("Help")
+            .setDescription(`
+                `)
+
+        message.reply({ embeds: [embed] })  
 })
